71085f7df536fdaf18f84a8e67973fdb6250a3a7
bn_tedjar@esi.dz <bn_tedjar@esi.dz@d550bdc6-1ee8-bd64-55db-dfb90dbc0a7f>
2013-10-06 12:12:53 +0000

    2 nd essai

---
+++ techDic/src/dz/esi/tp1Igl/MainActivity.java
@@ -0,0 +1,17 @@
+package dz.esi.tp1Igl;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import dz.esi.techdic.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+ TextView t ;
+    
+}
