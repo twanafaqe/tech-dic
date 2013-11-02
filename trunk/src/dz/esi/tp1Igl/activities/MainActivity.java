42a5a31172b05d65c5e7a76d64a16ab3e7c95be1
bn_tedjar@esi.dz <bn_tedjar@esi.dz@d550bdc6-1ee8-bd64-55db-dfb90dbc0a7f>
2013-11-02 23:12:15 +0000

    new dataBase et réglage du problème de la vitesse d'affichage

--- trunk/src/dz/esi/tp1Igl/activities/MainActivity.java
+++ trunk/src/dz/esi/tp1Igl/activities/MainActivity.java
@@ -5,78 +5,121 @@ package dz.esi.tp1Igl.activities;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 import android.annotation.SuppressLint;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+import android.widget.Toast;
+
 import com.example.techDic.R;
 
 import dz.esi.tp1Igl.modal.Word;
 import dz.esi.tp1Igl.service.DataBaseHandler;
 import dz.esi.tp1Igl.service.StableArrayAdapter;
 
-@SuppressLint("NewApi")
+/**
+ * <b>MainActivity c'est la classe qui affiche l'interface de l'application à l'utilisateur</b>
+ * 
+ * @author Aissani Amina & Tedjar Nour El imane
+ * @version 2.0
+ *
+ */
+
+
 @SuppressWarnings("deprecation")
 public class MainActivity extends TabActivity {
    
-	
-	       private TabHost monTabHost;
-	       DataBaseHandler db = new DataBaseHandler(this);
-	       Word [] tw;
-	       StableArrayAdapter ad;
-	       String [] word;
-	       String [] def;
+	       /**
+	     *  db instanciation de DataBaseHandler
+	     *  
+	     *  @see DataBaseHandler
+	     */
+	    DataBaseHandler db = new DataBaseHandler(this);
+	      /**
+	     * tw Tableau de Word 
+	     * 
+	     * @see Word
+	     */
+	    Word [] tw;
+	       /**
+	     * ad instanciation de StableArrayAdapter
+	     * 
+	     * @see StableArrayAdapter
+	     */
+	    StableArrayAdapter ad;
+	       /**
+	     * word un tableau de String,utiliser pour contenir des words des objets Word
+	     * dans la methode fillDB.
+	     * 
+	     * @see MainActivity#fillDB(String, int)
+	     * 
+	     */
+	    String [] word;
+	       /**
+	     *def un tableau de String,utiliser pour contenir des définitions des objets Word
+	     *dans la methode fillDB.
+	     *
+	     * @see MainActivity#fillDB(String, int)
+	     * 
+	     */
+	    String [] def;
 	       
-		   ListView list1;
-		   EditText def1;
-		   ListView list2;
-		   EditText def2;
-			   
-		   @Override
+	    
+	    public TabSpec fr;
+	    public TabSpec en;
+	    
+	    public Intent i2;
+	    
+	    public static Drawable enIm;
+	    /**
+	     * Le constructeur de la classe MainActivity par défaut.
+	     */
+	    public MainActivity()
+	    {
+	    	
+	    }
+		  
+		 @Override
 		   public void onCreate(Bundle savedInstanceState) {
+			   
 		      super.onCreate(savedInstanceState);
 		      setContentView(R.layout.activity_main);
-		
-	         
-		      /*list2=(ListView) findViewById(R.id.list2);
-		      def2=(EditText) findViewById(R.id.def2);*/
 		      
-		      
-		      
+		     // fillDB("DefEnh.txt", 0);
 		      TabHost tabHost = getTabHost();
 
 				// Tab pour le français
-				TabSpec fr = tabHost.newTabSpec("fr");
+				fr = tabHost.newTabSpec("fr");
 
-				Intent i1 = new Intent(this, SearchActivity.class);
+			    Intent i1 = new Intent(this, SearchActivity.class);
 				i1.putExtra("idOng", "Fr");
 
 				// non d'onglet
-				fr.setIndicator("Français",getResources().getDrawable(R.drawable.fre));
+				fr.setIndicator("Français",getResources().getDrawable(R.drawable.ic_launcher));
 				fr.setContent(i1);
 
 
 				// Tab pour l'anglais
-				TabSpec en = tabHost.newTabSpec("English");
+				 en = tabHost.newTabSpec("English");
 
-				Intent i2 = new Intent(this, SearchActivity.class);
-				i2.putExtra("idOng", "En");
+				 i2 = new Intent(this, SearchActivity.class);
+				 i2.putExtra("idOng", "En");
 				
-				//nom d'onglet
-				en.setIndicator("English", getResources().getDrawable(R.drawable.eng));
-				en.setContent(i2);
+				 //nom d'onglet
+				 en.setIndicator("English", enIm);
+				 en.setContent(i2);
 				
-				// Ajouter tous les TabSpec to TabHost
-				tabHost.addTab(fr);
-				tabHost.addTab(en);
+			   	// Ajouter tous les TabSpec to TabHost
+				 tabHost.addTab(fr);
+				 tabHost.addTab(en);
 				//initTabsAppearance(getTabWidget());
 		   }
 		  /* private void initTabsAppearance(TabWidget tabWidget) {
@@ -85,7 +128,20 @@ public class MainActivity extends TabActivity {
 					tabWidget.getChildAt(i).setBackgroundResource(R.drawable.tabbg);
 			}*/
 		   
-   public void fillDB(String filePath,int whatF)
+   /**
+    * fillDB cette méthode va nous permettre de remplir la base de données
+    * donc elle va etre appellè une seule fois.
+    * 
+ * @param filePath le nom du fichier qui contient les mots avec leurs définitions(Word).
+ * @param whatF un entier qui peut etre: 0 qui
+ *  signifié le fichier des Words français
+ *  ou 1 le fichier des Words englais.
+ * 
+ * @see Word
+ * 
+ */
+@SuppressLint("NewApi")
+public void fillDB(String filePath,int whatF)
 	{
 	
 		//0 pr englais 1 pr français
@@ -95,7 +151,7 @@ public class MainActivity extends TabActivity {
 			AssetManager am = MainActivity.this.getAssets();
 			InputStream is = am.open(filePath);
 			
-			scanner = new Scanner(is);
+			scanner = new Scanner(is,"UTF-8");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -107,10 +163,10 @@ public class MainActivity extends TabActivity {
 		char a; int i,j=0;
 		 
 		// On boucle sur chaque champ detecté
-		while (scanner.hasNextLine() && j<=7292 ) {
+		while (scanner.hasNextLine() && j<=5000 ) {
 			
-		
-		    String line = scanner.nextLine();
+		    
+			String line = scanner.nextLine();
 		   
 		
 		    Log.d("j=", String.valueOf(j));
@@ -151,9 +207,10 @@ public class MainActivity extends TabActivity {
 					}
 					else{
 						def=line.substring(i+2);
-						
+						Log.d("mot", w);
+						j++;
 						db.addWord(new Word(w,def), "Fr");
-					System.out.println(w+" \n "+ line.substring(i+2,line.length()-13).trim());
+					//System.out.println(w+" \n "+ line.substring(i+2,line.length()-13).trim());
 					}
 					//break;
 			}
@@ -163,12 +220,11 @@ public class MainActivity extends TabActivity {
 		scanner.close();
 }
 
-public TabHost getMonTabHost() {
-	return monTabHost;
-}
+/**
+ * Retourner le TabHost c-à-d les onglets de l'interface.
+ * 
+ * @return TabHost returner le TabHost
+ */
 
-public void setMonTabHost(TabHost monTabHost) {
-	this.monTabHost = monTabHost;
-}
 
 		}
--- trunk/src/dz/esi/tp1Igl/service/DataBaseHandler.java
+++ trunk/src/dz/esi/tp1Igl/service/DataBaseHandler.java
@@ -13,30 +13,136 @@ import android.util.Log;
 import dz.esi.tp1Igl.modal.Word;
 
 
+/**
+ * <p>DataBaseHandler permet de créer les tables dans la base de données (BD).</p>
+ * <p>Les tables crées dans la BD sont les suivants : </p>
+ * <lu>
+ * <li>TABLE_FRENCH </li>
+ * <li>TABLE_ENGLISH</li>
+ * <li>TABLE_COUNT</li>
+ * </lu>
+ *  
+ * @author Aissani Amina & Tedjar Nour El Imane
+ * 
+ * @version 3.0
+ *
+ */
+
 @SuppressLint("NewApi")
 public class DataBaseHandler extends SQLiteOpenHelper {
 
-	// Database Version
+	
+	/**
+	 * DATABASE_VERSION contient le numéro de la version de la BD.
+	 */
 	private static final int DATABASE_VERSION = 1;
 
-	// Database Name
-	private static final String DATABASE_NAME = "Base3Fin";
+	
+	/**
+	 * DATABASE_NAME par lequelle on attribue un nom à la BD.
+	 */
+	private static final String DATABASE_NAME = "dB1";
 
-	// tables name
+	/**
+	 * <p>TABLE_FRENCH La table qui va contenir les Words en français.</p>
+	 * <p>Cette table est caractérisée par: </p>
+	 * <lu>
+	 * <li> KEY_ID_WORD 
+	 * @see DataBaseHandler#KEY_ID_WORD </li>
+	 * <li> KEY_WORD 
+	 * @see DataBaseHandler#KEY_WORD </li>
+	 * <li> KEY_DEF
+	 * @see DataBaseHandler#KEY_DEF </li>
+	 * @see Word
+	 */
 	private static final String TABLE_FRENCH = "Fr";
+	/**
+	 * TABLE_ENGLISH La table qui va contenir les Words en englais.
+	 * <p>Cette table est caractérisée par: </p>
+	 * <lu>
+	 * <li> KEY_ID_WORD 
+	 * @see DataBaseHandler#KEY_ID_WORD </li>
+	 * <li> KEY_WORD 
+	 * @see DataBaseHandler#KEY_WORD </li>
+	 * <li> KEY_DEF
+	 * @see DataBaseHandler#KEY_DEF </li>
+	 * @see Word
+	 */
 	private static final String TABLE_ENGLISH = "En";
+	/**
+	 * TABLE_COUNT La table qui associé à chaque lettre de l'alphabets ses mots existants dans 
+	 * la BD en récupérant leurs Ids de la  TABLE_ENGLISH et TABLE_FRENCH.
+	 *  
+	 */
 	private static final String TABLE_COUNT = "Count";
 
-	// Table Columns names
+	/**
+	 * KEY_WORD c'est le nom de la colonne qui va contenir les words des objets 
+	 * Words dans la BD (le mot qu'on cherche définir).
+	 * 
+	 * 
+	 * @see Word
+	 */
+	
 	private static final String KEY_WORD = "word";
+	/**
+	 * KEY_ID_WORD c'est le nom de la colonne qui va contenir les Ids des
+	 * objets Words dans la BD(identifiant unique pour chaque mot dans la table).
+	 * 
+	 * @see Word
+	 */
 	private static final String KEY_ID_WORD = "idWord";
+	/**
+	 * KEY_DEF  c'est le nom de la colonne qui va contenir les définitions des
+	 * objets Words dans la BD.
+	 * 
+	 * @see Word
+	 */
 	private static final String KEY_DEF = "def";
 	
+	/**
+	 * KEY_ID_LETTER c'est le nom de la colonne dans la BD qui va contenir 
+	 * les Ids des lettres de l'alphabets.
+	 * 
+	 * @see Word
+	 */
 	private static final String KEY_ID_LETTER = "idLetter";
+	/**
+	 * KEY_LETTER c'est le nom de la colonne dans la BD qui va contenir 
+	 * les lettres de l'alphabets.
+	 * 
+	 * 
+	 */
 	private static final String KEY_LETTER = "letter";
+	/**
+	 * KEY_FRE c'est le nom de la colonne dans la BD qui va contenir 
+	 * les Ids de tous les words (mots) français qui commencent par la
+	 * lettre qui est définit dans KEY_LETTER.
+	 * 
+	 * 
+	 */
 	private static final String KEY_FRE = "fre";
+	/**
+	 * KEY_ENGc'est le nom de la colonne dans la BD qui va contenir 
+	 * les Ids de tous les words (mots) englais qui commencent par la
+	 * lettre qui est définit dans KEY_LETTER.
+	 * 
+	 *
+	 */
 	private static final String KEY_ENG = "eng";
 
+	/**
+	 * Cette variable est utilisée pour construire le progressBar
+	 * dans SearchActivity pour attendre la fin d'obtention des
+	 * mot correspondant à la lettre tapée
+	 */
+	private int progress=0;
+	
+	/**
+	 * DataBaseHandler le constructeur de la classe
+	 * @param context pour indiquer dans quelle activity est appellé.
+	 */
+	
 	public DataBaseHandler(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 		// TODO Auto-generated constructor stub
@@ -57,7 +163,7 @@ public class DataBaseHandler extends SQLiteOpenHelper {
 		
 		String CREATE_COUNT_TABLE = "CREATE TABLE " + TABLE_COUNT + "("
 				+ KEY_ID_LETTER + " INTEGER PRIMARY KEY," + KEY_LETTER + " TEXT,"
-				+ KEY_FRE + " INTEGER " + KEY_ENG + " INTEGER "+ ")";
+				+ KEY_FRE + " INTEGER," + KEY_ENG + " INTEGER "+ ")";
 		db.execSQL(CREATE_COUNT_TABLE);
 
 	}
@@ -67,6 +173,7 @@ public class DataBaseHandler extends SQLiteOpenHelper {
 		// TODO Auto-generated method stub
 		db.execSQL("DROP TABLE IF EXISTS " + TABLE_FRENCH);
 		db.execSQL("DROP TABLE IF EXISTS " + TABLE_ENGLISH);
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_COUNT);
 
 		// Create tables again
 		onCreate(db);
@@ -75,11 +182,25 @@ public class DataBaseHandler extends SQLiteOpenHelper {
 
 	// recuperer ts les mots
 
-	public List<Word> getAllWords(String tabName) {
+	/**
+	 * getAllWords la methode qui parcourt toute la table 
+	 * que son nom est indiqué en paramètres(tabName) dans la BD
+	 * et récupère tous les Words dont l'identifiant est compris 
+	 * entre begin et end,cela se fait sous forme d'une liste. 
+	 * @param tabName le nom de la table qu'on veut extraire ses Words.
+	 * @param begin  l'identifiant du premier mot
+	 * @param end  l'identifiant du mot dernier
+	 *            
+	 * @return une liste de Word
+	 * @see Word
+	 */
+	public List<Word> getAllWords(String tabName,int begin,int end) {
 		
 		List<Word> wordList = new ArrayList<Word>();
 		// Select All Query
-		String selectQuery = "SELECT  * FROM " + tabName;
+		String selectQuery = "SELECT * FROM "+tabName+
+				" WHERE "+KEY_ID_WORD +" >= "+String.valueOf(begin) +" AND " +KEY_ID_WORD+" < "
+				+String.valueOf(end);
 
 		SQLiteDatabase db = this.getWritableDatabase();
 		Cursor cursor = db.rawQuery(selectQuery, null);
@@ -101,6 +222,13 @@ public class DataBaseHandler extends SQLiteOpenHelper {
 		return wordList;
 	}
 	
+	/**
+	 * addWord permt d'ajouter un Word dans la table tabName.
+	 * @param w  indique un Word      
+	 * @param tabName le nom de la table ou on veut ajouter le Word w.
+	 * 
+	 * @see Word
+	 */
 	public void addWord(Word w, String tabName) {
 		
 		SQLiteDatabase db = this.getWritableDatabase();
@@ -115,8 +243,16 @@ public class DataBaseHandler extends SQLiteOpenHelper {
 		db.close();
 	}
 
-	// Getting single word
+	
 
+	/**
+	 * getWord permet de récupérer un Word de la table tabName.
+	 * @param idWord le Id du Word qu'on veut récupérer.
+	 * @param tabName le nom de la table dans lequelle on va récupérer le Word.
+	 * @return Word  le Word 
+	 * 
+	 * @see Word
+	 */
 	public Word getWord(int idWord, String tabName) {
 		SQLiteDatabase db = this.getReadableDatabase();
 		Word w = null;
@@ -135,25 +271,41 @@ public class DataBaseHandler extends SQLiteOpenHelper {
     //getting all words beginnig with a letter
 	
 	
+	/**
+	 * getWords nous permet de récupèrer une liste de tous les Words  
+	 * qui commence par la lettre (lettre) indiquée en paramètre  de la table 
+	 * tabName ainsi indiqué en paramètre.
+	 * @param tabName le nom de la table ou on veut récupérer les Words.
+	 * @param letter  une des lettres de l'alphabets.
+	 * @return list<Word>  le résultats sous forme d'une liste de Word.
+	 * 
+	 * @see Word
+	 */
 	@SuppressWarnings({ })
 	public List<Word> getWords(String tabName, char letter){
 		
-		int i,beg=1,end=1; List<Word> wordsList=new ArrayList<Word>();
+		int beg=1,end=1; List<Word> wordsList=new ArrayList<Word>();
 				
 		if(letter!='a') beg=getCountByLetter(letter-1, tabName);
 		end=getCountByLetter(letter, tabName);
 		
 		Log.d("end", String.valueOf(end));
 		
-		for(i=beg;i<end;i++){
-			wordsList.add( getWord(i,tabName));
-		}
+		wordsList=getAllWords(tabName, beg, end);
 		
 		return wordsList;
 	}
 	
-	//getting the word's number of a letter
 	
+	
+	/**
+	 * getCountByLetter compte le nombre de Word dans la table tabName indiqué en paramètre
+	 * qui commence par une lettre spécifiée ainsi  en paramètre.
+	 * @param letter la lettre est présentée par son code Ascci sous forme d'un entier.
+	 * @param tabName la table le nom de la table qu'on va parcourir.
+	 * @return un entier qui représente le nombre de mot dans la table tabName 
+	 *         qui commence par la lettre lettre.
+	 */
 	public int getCountByLetter(int letter, String tabName){
 		
 		int i,k=0,ret=0;
@@ -164,21 +316,37 @@ public class DataBaseHandler extends SQLiteOpenHelper {
 			if (i==letter) break;
 		}
 			SQLiteDatabase db = this.getReadableDatabase();
-			Cursor cursor = db.query(TABLE_COUNT, new String[] { KEY_ID_LETTER,KEY_LETTER,
-					KEY_FRE, KEY_ENG },
+			Cursor cursor = db.query(TABLE_COUNT, new String[] {  KEY_ENG,KEY_ID_LETTER,
+					KEY_LETTER,KEY_FRE },
 					KEY_ID_LETTER + "=?", new String[] { String.valueOf(k) }, null,
 					null, null, null);
 			if (cursor != null){
 				cursor.moveToFirst();
 				if (tabName.equals("Fr")) 
 					{
-					ret=Integer.parseInt(cursor.getString(2));
+					ret=Integer.parseInt(cursor.getString(3));
 					Log.d("ret", String.valueOf(ret));
 					}
-				else ret= Integer.parseInt(cursor.getString(3));}
+				else ret= Integer.parseInt(cursor.getString(0));}
 			
 	           
 		return ret;
 
 }
+	
+	
+	
+	/**
+	 * @return progress
+	 */
+	public int getProgress() {
+		return progress;
+	}
+
+	/**
+	 * @param progress
+	 */
+	public void setProgress(int progress) {
+		this.progress = progress;
+	}
 }
